f2350958653106968d1e0786318252c11b49e171
xwildeyes <xwildeyes@gmail.com>
2016-04-16 12:34:44 +0300

    implement styles changes for team mates

--- leaderboard.js
+++ leaderboard.js
@@ -26,7 +26,7 @@ if (Meteor.isClient) {
   });
 
   Template.buygood.helpers({
-    products:_ => Template.instance().products.get(),
+    products: _ => Template.instance().products.get(),
     adjustLength: str => str.slice(0,8) + '...',
     price:item => item.sellingStatus[0].currentPrice[0]['__value__'],
     currency:item => item.sellingStatus[0].currentPrice[0]['@currencyId']
@@ -60,27 +60,51 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
   function extractPrice(item) {
+    const val = item.sellingStatus[0].currentPrice[0]
     return {
-      price: item.sellingStatus.currentPrice['@currencyId'],
-      curreny: item.sellingStatus.currentPrice['__value__'],
+      price: val['__value__'],
+      currency: val['@currencyId'],
     }
   }
 
   function getItemsEbay(result) {
     // const items = result.findItemsByKeywords[0].searchResult[0].item
     return result.findItemsByKeywordsResponse[0].searchResult[0].item
-
-    return items.map(item => {
-      return Object.assign({
-        name: item.title,
-        pictureURL: item.galleryURL,
-        price: item.sellingStatus,
-      }, extractPrice(item))
-    })
   }
 
-  function getAPIURL(keyword) {
-    return `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=DanielKh-BuyGood-PRD-4e805b337-9ea4cc33&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${keyword}&sortOrder=PricePlusShippingLowest&paginationInput.entriesPerPage=1000&paginationInput.pageNumber=1`
+  function Item(item) {
+    return Object.assign({
+        name: item.title[0],
+        pictureURL: item.galleryURL[0],
+      }, extractPrice(item))
+  }
+
+  function lowestPriceURL(keyword, minPrice) {
+    return (`http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=DanielKh-BuyGood-PRD-4e805b337-9ea4cc33&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${keyword}&itemFilter.name=MinPrice&itemFilter.value=${minPrice}&sortOrder=PricePlusShippingLowest`) //&paginationInput.entriesPerPage=1000&paginationInput.pageNumber=1
+  }
+  function reasonablePriceURL(keyword) {
+    return `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=DanielKh-BuyGood-PRD-4e805b337-9ea4cc33&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${keyword}`
+  }
+
+  function APICall(url) {
+    const res = HTTP.get(url)
+    return JSON.parse(res.content)
+  }
+
+  function getReasonablePrice(keyword) {
+    const data = APICall(reasonablePriceURL(keyword))
+    const items = getItemsEbay(data)
+
+    return Item(items[0]).price * 0.25
+  }
+
+  function getLowestPriceItems(keyword) {
+    // const res = HTTP.post('https://gifted-beaver-k69b.rapidapi.io/ebay-find-items', {data:{keyword}})
+    const reasonablePrice = getReasonablePrice(keyword)
+    const data = APICall(lowestPriceURL(keyword, reasonablePrice))
+    // console.log(JSON.stringify(data.findItemsByKeywordsResponse[0].errorMessage))
+    // return console.log(res)
+    return getItemsEbay(data)
   }
 
   Meteor.methods({
@@ -91,15 +115,6 @@ if (Meteor.isServer) {
 
     //   doneFn(null, products)
     // }),
-    products: Meteor.wrapAsync((keyword, doneFn) => {
-      // const res = HTTP.post('https://gifted-beaver-k69b.rapidapi.io/ebay-find-items', {data:{keyword}})
-      const res = HTTP.get(getAPIURL(keyword))
-      const data = JSON.parse(res.content)
-      // return console.log(res)
-      const products = getItemsEbay(data)
-      // const products = getItemsEbay(res.data)
-
-      doneFn(null, products)
-    })
+    products: keyword => getLowestPriceItems(keyword)
   })
 }
